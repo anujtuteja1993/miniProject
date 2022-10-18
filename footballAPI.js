67b76ed18742b6dc4aa5e0ed5a1ffbab25b730db
jobatkinIOD <jo.batkin@institutedata.com>
2022-10-18 11:57:44 +1000

    reviewed, added suggestions

--- footballAPI.js
+++ footballAPI.js
@@ -1,3 +1,5 @@
+const matchVideos = new Map()
+
 fetch(
     "https://www.scorebat.com/video-api/v3/feed/?token=MjkxNzdfMTY2NTQ4ODE1Nl9jMWZlM2QxZmNiODMzMTE3MmQ1MWU2MTAzMjZkNDMwY2NiMjUxZTQ0"
 )
@@ -20,8 +22,34 @@ function addCard(card) {
     template.querySelector(".card-text").innerText = card.competition;
     template.getElementById("thumbnail").src = card.thumbnail;
     template.getElementById("matchDetails").href = card.matchviewUrl;
+    
+    //store the video embed codes and when you click the button, show the matching one
+    template.getElementById("matchVideoButton").setAttribute('onclick', 'showVideo("'+card.videos[0].id+'")');
+    matchVideos.set(card.videos[0].id, card.videos[0].embed)
+    
     document.querySelector("#car-list").appendChild(template);
     //template.getElementById("thumbnail").src = card.videos[0].embed;
-    var videoEmbed = card.videos[0].embed;
-    console.log(videoEmbed);
+    //var videoEmbed = card.videos[0].embed;
+    //console.log(videoEmbed);
 }
+
+function showVideo(embedId)
+{
+    document.getElementById('matchVideo').innerHTML = matchVideos.get(embedId)
+}
+
+//this function just changes the nav link to show if you're logged in or not
+function checkLogin() {
+    let username = localStorage.getItem("name");
+    if (username) {
+        document.getElementById('loginLink').setAttribute('href', '#');
+        document.getElementById('loginLink').innerText = 'Hello '+username;
+    }
+    else
+    {
+        document.getElementById('loginLink').setAttribute('href', 'login.html');
+        document.getElementById('loginLink').innerText = 'Login';
+    }
+}
+
+window.onload = checkLogin;
